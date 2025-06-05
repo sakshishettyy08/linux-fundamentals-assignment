84567f4fa08533f1e3d728e955cce2111a39a2ec
sakshishettyy08 <145077329+sakshishettyy08@users.noreply.github.com>
2025-06-05 12:52:22 -0400

    Update health_check.sh

--- Linux-Assignment/health_check.sh
+++ Linux-Assignment/health_check.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "Load Average" >> /var/tmp/health_log.log
-uptime | awk -F'load average:' '{print $2}' >> /var/tmp/health_log.log
 
-echo "Memory Availability" >> /var/tmp/health_log.log
-free -h | awk 'NR>1 {print $1 " " $4}' >> /var/tmp/health_log.log
+LOGFILE="/var/tmp/health_log.log"
 
-echo "SSH is running" >> /var/tmp/health_log.log
-systemctl status ssh >> /var/tmp/health_log.log
+echo "--- Health Check Report ----" >> "$LOGFILE"
+
+echo "Load Average" >> "$LOGFILE"
+uptime | awk -F'load average:' '{print $2}' >> "$LOGFILE"
+
+echo "Memory Availability" >> "$LOGFILE"
+free -h | awk 'NR>1 {print $1 " " $4}' >> "$LOGFILE"
+
+echo "SSH is running" >> "$LOGFILE"
+systemctl status ssh >> "$LOGFILE"
